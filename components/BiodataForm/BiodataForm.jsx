69a7b6a40aef9c4af028c6302e8b1c407633704a
meet3475 <meetdobariya480@gmail.com>
2025-04-28 13:08:50 +0530

    done icon delete

--- components/BiodataForm/BiodataForm.jsx
+++ components/BiodataForm/BiodataForm.jsx
@@ -1303,7 +1303,7 @@ const BiodataForm = ({ scrollToTemplates }) => {
                 {/* Delete Button */}
                 <button
                     type="button"
-                    className="ml-2 w-9 h-9 rounded-full border border-[#9E2665] flex items-center justify-center text-black hover:text-white hover:bg-[#9E2665] transition-colors duration-200"
+                    className="p-1 ml-2 w-9 h-9 rounded-full border border-[#9E2665] flex items-center justify-center text-black hover:text-white hover:bg-[#9E2665] transition-colors duration-200"
                     onClick={() => deleteField(section, fieldName)}
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-trash2 "><path d="M3 6h18"></path><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path><line x1="10" x2="10" y1="11" y2="17"></line><line x1="14" x2="14" y1="11" y2="17"></line></svg>
