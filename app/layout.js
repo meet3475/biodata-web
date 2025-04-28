6de988a31ec91a3d8758c5df23c398c26de45311
meet3475 <meetdobariya480@gmail.com>
2025-04-28 15:47:12 +0530

    done error by canonical

--- app/layout.js
+++ app/layout.js
@@ -20,6 +20,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="canonical" href="https://biodataweb.vercel.app" />
+      </head>
       <body>
         {children}
       </body>
